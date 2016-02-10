15b895f80fbb9e1158d0b72319b60a23c15f20f6
Sergey <sergio_lebedev@yahoo.it>
2016-02-10 16:04:10 +0100

    sprint 1 e test

--- js/app.js
+++ js/app.js
@@ -12,8 +12,19 @@ var ParlaModel = {
     *         null, if language is not valid
     */
    getLanguageScore : function (phrase, language){
-     // write your code here, but don't add any additional 
-     // parameters to the the function
+        var score = 0;
+       for(var j in this.langs){
+           if(this.langs[j].name == language){
+        for(var i in this.langs[j].dictionary){
+         
+            if (phrase.toLowerCase().search(this.langs[j].dictionary[i]) != -1){
+             score++;
+            }
+        }
+       }
+       }
+       console.log(score);
+       return score;
    },
   
    /* Returns the name of the language in which the phrase
@@ -21,8 +32,76 @@ var ParlaModel = {
     * returns the language with higher *score*. 
     */
    detectLanguage : function (phrase) {
-     // write your code here, but don't add any additional 
-     // parameters to the the function     
+       var maxScore = "";
+       var score = 0;
+       for(var k in this.langs){
+           score = this.getLanguageScore(phrase, this.langs[k].name);
+           if(score > maxScore){maxScore = this.langs[i].name;}
+        }
+       
+       return maxScore;
    }
          
 };
+
+
+
+var ParlaView = {
+    
+    /* presents first view and enables click functionality */
+    init: function(){
+        
+        var self = this;
+        
+        $(".lang-name").empty();
+        $(".lang-name").hide();
+        
+        
+    },
+    
+    /* updates HTML with processed information */
+    render: function(stuff){
+        
+        $(".lang-name").empty();
+        $(".lang-name").show();
+            
+        $(".lang-name").append(stuff);
+        
+    }
+    
+};
+
+var ParlaController = {
+    
+    /* initializes app on new run */
+    init: function(){
+        ParlaView.init();
+    },
+    
+    /* method to retrieve data from Model */
+    getLanguages: function(){
+        return ParlaModel.langs();
+    },
+    
+    /* actions to be performed on click */
+    action: function(phrase){
+        return ParlaModel.detectLanguage(phrase);
+    },
+    
+    render: function(ris){
+        
+        ParlaView.render(ris);
+    }
+};
+
+
+/* enable functionalities after document fully loaded HTML and scripts */
+$(document).ready(function(){
+    ParlaController.init();
+    
+    $("button").click( function() {
+            var ris = ParlaController.action( $("input").text() );
+            ParlaController.render(ris);
+        });
+        
+});
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -2,9 +2,51 @@
 /* Try to maximise the coverage of the ParlaModel object */
 
 describe("Parla", function() {
-
-  it("should...", function() {  
+    describe("ParlaModel getLanguageScore", function() {
+  it("should not award scores when not detecting any words", function() { 
+      
+      expect(ParlaModel.getLanguageScore("x","English")).toBe(0);
     
-  });    
+  });   
+        
+        it("should award a score when detecting 1 word", function() { 
+      
+      expect(ParlaModel.getLanguageScore("the","English")).toBe(1);
+    
+  }); 
+        it("should award 2 scores when detecting 2 words", function() { 
+      
+      expect(ParlaModel.getLanguageScore("the best of","English")).toBe(2);
+    
+  }); 
+        
+    });
+    
+    
+    describe("ParlaModel detectLanguage", function(){
+        
+       it("should not detect any language properly", function() { 
+      
+      expect(ParlaModel.detectLanguage("x")).toBe(null);
+    
+  });  
+        it("should detect a language correctly in case of 1 match", function() { 
+      
+      expect(ParlaModel.detectLanguage("of xxxx")).toBe("English");
+    
+  }); 
+        it("should detect a language correctly with 2 matches", function() { 
+      
+      expect(ParlaModel.detectLanguage("of the best xxx")).toBe("English");
+    
+  }); 
+        it("should detect a language properly when there are conflicts", function() { 
+      
+      expect(ParlaModel.detectLanguage("non of the best xxx")).toBe("English");
+    
+  }); 
+        
+    
+    });
  
 });
